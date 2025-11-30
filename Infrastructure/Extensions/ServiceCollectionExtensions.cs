2ed4167997e81b902b5df25e46258b3c4458145f
Lorik Manaj <lorikmanaj@hotmail.com>
2025-11-30 14:50:00 +0100

    Added ServiceCollection and WebApp Extensions

--- Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,47 @@
-﻿namespace Cyviz.Infrastructure.Extensions
+﻿using Cyviz.Core.Application.Interfaces;
+using Cyviz.Infrastructure.Caching;
+using Cyviz.Infrastructure.Database;
+using Cyviz.Infrastructure.Middlewares;
+using Cyviz.Infrastructure.ProtocolAdapters_PLACEHOLDERS_;
+using Cyviz.Infrastructure.Repositories.Entities;
+using Cyviz.SignalR.Pipelines;
+using Cyviz.SignalR.Workers;
+
+namespace Cyviz.Infrastructure.Extensions
 {
-    public class ServiceCollectionExtensions
+    public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            // SQLite DB
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("CyvizAPI"));
+            });
+
+            // Add Repositories
+            services.AddScoped<IDeviceRepository, DeviceRepository>();
+            services.AddScoped<ITelemetryRepository, TelemetryRepository>();
+            services.AddScoped<IDeviceCommandRepository, DeviceCommandRepository>();
+
+            // Add Caching
+            services.AddMemoryCache();
+            services.AddSingleton<IDeviceSnapshotCache, DeviceSnapshotCache>();
+
+            // Add Pipeline + Workers
+            services.AddSingleton<ICommandPipeline, CommandChannelPipeline>();
+            services.AddSingleton<IWorkerManager, WorkerManager>();
+
+            // Protocol Adapters (placeholders)
+            //services.AddScoped<IDeviceProtocolAdapter, EdgeSignalRAdapter>();
+
+            // Middlewares
+            services.AddScoped<RequestLoggingMiddleware>();
+            services.AddScoped<ApiKeyMiddleware>();
+            services.AddScoped<ExceptionMiddleware>();
+
+            return services;
+        }
     }
 }
--- Infrastructure/Extensions/WebApplicationExtensions.cs
+++ Infrastructure/Extensions/WebApplicationExtensions.cs
@@ -1,6 +1,33 @@
-﻿namespace Cyviz.Infrastructure.Extensions
+﻿using Cyviz.Core.Application.Interfaces;
+using Cyviz.Infrastructure.Database;
+
+namespace Cyviz.Infrastructure.Extensions
 {
-    public class WebApplicationExtensions
+    public static class WebApplicationExtensions
     {
+        public static WebApplication RunMigrations(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            var repo = scope.ServiceProvider.GetRequiredService<IDeviceRepository>();
+
+            db.Database.Migrate();
+            db.Database.EnsureCreated();
+
+            // Seed devices on first run
+            if (!repo.AnyDevices())
+            {
+                var seeder = scope.ServiceProvider.GetRequiredService<IDeviceSeeder>();
+                seeder.SeedDevicesAsync().Wait();
+            }
+
+            return app;
+        }
+
+        public static void StartSignalRWorkers(this WebApplication app)
+        {
+            var workerManager = app.Services.GetRequiredService<IWorkerManager>();
+            workerManager.StartWorkers();
+        }
     }
 }
