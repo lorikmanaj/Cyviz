cf9e34719f033c8cdce3d4995b3660954ab575ad
Lorik Manaj <lorikmanaj@hotmail.com>
2025-11-30 18:06:05 +0100

    Added Base DeviceCircuitBreakerRegisry to be referenced in woker

---
+++ Core/Domain/Services/DeviceCircuitBreakerRegistry.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Concurrent;
+
+namespace Cyviz.Core.Domain.Services
+{
+    public interface IDeviceCircuitBreakerRegistry
+    {
+        ICircuitBreaker GetCircuitBreaker(string deviceId);
+    }
+
+    public class DeviceCircuitBreakerRegistry(CircuitBreakerOptions? options = null) : IDeviceCircuitBreakerRegistry
+    {
+        private readonly ConcurrentDictionary<string, ICircuitBreaker> _breakers = new();
+
+        private readonly CircuitBreakerOptions _options = options ?? new CircuitBreakerOptions();
+
+        public ICircuitBreaker GetCircuitBreaker(string deviceId)
+        {
+            return _breakers.GetOrAdd(deviceId, _ => new CircuitBreaker(_options));
+        }
+    }
+}
