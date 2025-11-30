b0255c665c2c15d1803a24fb14108042b3e8fd15
Lorik Manaj <lorikmanaj@hotmail.com>
2025-11-30 16:53:03 +0100

    Added custom exceptions

---
+++ Core/Application/Exceptions/CommandQueueFullException.cs
@@ -0,0 +1,10 @@
+﻿namespace Cyviz.Core.Application.Exceptions
+{
+    public class CommandQueueFullException : Exception
+    {
+        public CommandQueueFullException()
+            : base("Command queue is full. Please retry later.")
+        {
+        }
+    }
+}
---
+++ Core/Application/Exceptions/PipelineFullException.cs
@@ -0,0 +1,6 @@
+﻿namespace Cyviz.Core.Application.Exceptions
+{
+    public class PipelineFullException(string message) : Exception(message)
+    {
+    }
+}
