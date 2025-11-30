477753c4a155c853985414d8e09a78a89f75030e
Lorik Manaj <lorikmanaj@hotmail.com>
2025-11-30 16:12:49 +0100

    Added Keyset Paginator extension

---
+++ Core/Application/Models/Pagination/KeysetPageResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Cyviz.Core.Application.Models.Pagination
+{
+    public class KeysetPageResult<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public string? NextCursor { get; set; }
+    }
+}
---
+++ Infrastructure/Extensions/KeysetPaginationExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Cyviz.Core.Application.Models.Pagination;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Cyviz.Infrastructure.Extensions
+{
+    public static class KeysetPaginationExtensions
+    {
+        public static async Task<KeysetPageResult<T>> ToKeysetPageAsync<T, TKey>(
+            this IQueryable<T> query,
+            Expression<Func<T, TKey>> keySelector,
+            TKey? after,
+            int pageSize)
+        {
+            if (pageSize < 1) pageSize = 1;
+            if (pageSize > 50) pageSize = 50;
+
+            // If cursor is provided → filter
+            if (after != null)
+            {
+                var param = keySelector.Parameters[0];
+                var compare = Expression.GreaterThan(
+                    keySelector.Body,
+                    Expression.Constant(after));
+
+                var lambda = Expression.Lambda<Func<T, bool>>(compare, param);
+                query = query.Where(lambda);
+            }
+
+            // Sort ASC by key
+            query = query.OrderBy(keySelector);
+
+            var items = await query.Take(pageSize).ToListAsync();
+
+            TKey? nextCursor = default;
+
+            if (items.Count != 0)
+            {
+                var last = items.Last();
+                nextCursor = (TKey)keySelector.Compile().Invoke(last);
+            }
+
+            return new KeysetPageResult<T>
+            {
+                Items = items,
+                NextCursor = nextCursor?.ToString()
+            };
+        }
+    }
+}
