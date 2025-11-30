733d313d629c5769e29322dbbfba829b0d528c19
Lorik Manaj <lorikmanaj@hotmail.com>
2025-11-30 15:26:51 +0100

    Added BaseRepo logic (Repository Pattern)

--- Infrastructure/Repositories/Generic/BaseRepository.cs
+++ Infrastructure/Repositories/Generic/BaseRepository.cs
@@ -1,6 +1,63 @@
-﻿namespace Cyviz.Infrastructure.Repositories.Generic
+﻿using Cyviz.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Cyviz.Infrastructure.Repositories.Generic
 {
-    public class BaseRepository : IBaseRepository
+public class BaseRepository<T> : IBaseRepository<T> where T : class
     {
+        protected readonly ApplicationDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public virtual async Task<T?> GetByIdAsync(object id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public virtual IQueryable<T> Query()
+        {
+            return _dbSet.AsQueryable();
+        }
+
+        public virtual async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public virtual async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        public virtual void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public virtual void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public virtual async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public virtual IQueryable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+
+        public virtual async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
     }
 }
--- Infrastructure/Repositories/Generic/IBaseRepository.cs
+++ Infrastructure/Repositories/Generic/IBaseRepository.cs
@@ -1,6 +1,22 @@
-﻿namespace Cyviz.Infrastructure.Repositories.Generic
+﻿using System.Linq.Expressions;
+
+namespace Cyviz.Infrastructure.Repositories.Generic
 {
-    public interface IBaseRepository
+    public interface IBaseRepository<T> where T : class
     {
+        Task<T?> GetByIdAsync(object id);
+
+        IQueryable<T> Query();
+
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        void Update(T entity);
+        void Remove(T entity);
+
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+
+        Task<int> SaveChangesAsync();
     }
 }
