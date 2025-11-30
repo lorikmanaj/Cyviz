62887add491525cc07b42e026696d3550ddeecf0
Lorik Manaj <lorikmanaj@hotmail.com>
2025-11-30 14:50:08 +0100

    Added Device Seeder

---
+++ Infrastructure/Database/Seeders/DeviceSeeder.cs
@@ -0,0 +1,15 @@
+﻿namespace Cyviz.Infrastructure.Database.Seeders
+{
+    public interface IDeviceSeeder
+    {
+        Task SeedDevicesAsync();
+    }
+
+    public class DeviceSeeder : IDeviceSeeder
+    {
+        public Task SeedDevicesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
