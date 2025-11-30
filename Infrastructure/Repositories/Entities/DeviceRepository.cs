8037c64c194578899376a2095804a1476218c5bd
Lorik Manaj <lorikmanaj@hotmail.com>
2025-11-30 16:17:14 +0100

    Added Pagination implementation in DeviceRepo

--- Infrastructure/Repositories/Entities/DeviceRepository.cs
+++ Infrastructure/Repositories/Entities/DeviceRepository.cs
@@ -1,11 +1,25 @@
-﻿using Cyviz.Core.Application.Repositories;
+﻿using Cyviz.Core.Application.Models.Pagination;
+using Cyviz.Core.Application.Repositories;
 using Cyviz.Core.Domain.Entities;
 using Cyviz.Infrastructure.Database;
+using Cyviz.Infrastructure.Extensions;
 using Cyviz.Infrastructure.Repositories.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cyviz.Infrastructure.Repositories.Entities
 {
     public class DeviceRepository(ApplicationDbContext context) : BaseRepository<Device>(context), IDeviceRepository
     {
+        public async Task<KeysetPageResult<Device>> GetDevicesKeysetAsync(string? after, int pageSize)
+        {
+            IQueryable<Device> query = _context.Devices.AsNoTracking();
+
+            // Cursor conversion: if null -> pass null
+            return await query.ToKeysetPageAsync(
+                keySelector: d => d.Id,  // Keyset: device-01, device-02
+                after: string.IsNullOrWhiteSpace(after) ? null : after,
+                pageSize: pageSize
+            );
+        }
     }
 }
--- Infrastructure/Services/DeviceService.cs
+++ Infrastructure/Services/DeviceService.cs
@@ -1,6 +1,44 @@
-﻿namespace Cyviz.Infrastructure.Services
+﻿using AutoMapper;
+using Cyviz.Core.Application.DTOs.Device;
+using Cyviz.Core.Application.Models.Pagination;
+using Cyviz.Core.Application.Repositories;
+using Cyviz.Core.Application.Services;
+
+namespace Cyviz.Infrastructure.Services
 {
-    public class DeviceService
+    public class DeviceService(IDeviceRepository repo, IMapper mapper) : IDeviceService
     {
+        private readonly IDeviceRepository _repo = repo;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<KeysetPageResult<DeviceListDto>> GetDevicesAsync(string? after, int pageSize)
+        {
+            var page = await _repo.GetDevicesKeysetAsync(after, pageSize);
+
+            return new KeysetPageResult<DeviceListDto>
+            {
+                Items = _mapper.Map<IEnumerable<DeviceListDto>>(page.Items),
+                NextCursor = page.NextCursor
+            };
+        }
+
+        public async Task<DeviceDetailDto> GetDeviceByIdAsync(string id)
+        {
+            var device = await _repo.GetByIdAsync(id)
+                ?? throw new Exception("Device not found");
+
+            return _mapper.Map<DeviceDetailDto>(device);
+        }
+
+        public async Task UpdateDeviceAsync(string id, DeviceUpdateDto dto)
+        {
+            var device = await _repo.GetByIdAsync(id)
+                ?? throw new Exception("Device not found");
+
+            _mapper.Map(dto, device);
+
+            _repo.Update(device);
+            await _repo.SaveChangesAsync();
+        }
     }
 }
